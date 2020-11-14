681b74fc0ffe5e07f01b026dbf643623eef18f4d
emazurov <emazurov@ozon.ru>
2020-11-15 02:11:07 +0300

    [master] little refactor

--- src/dictionaries/declensions.ts
+++ src/dictionaries/declensions.ts
@@ -1,12 +1,16 @@
 type TextForms = [string, string, string] // ['минута', 'минуты', 'минут']
+type Dictionary = Readonly<{
+  [key in string | number]: string;
+}>;
+const createDeclensions = (textForms: TextForms) => new Proxy<Dictionary>({}, {
+  get(target, p) {
+    const n = Math.abs(Number(p)) % 100;
+    const n1 = n % 10;
+    if (n > 10 && n < 20) { return textForms[2]; }
+    if (n1 > 1 && n1 < 5) { return textForms[1]; }
+    if (n1 === 1) { return textForms[0]; }
+    return textForms[2];
+  },
+});
 
-const createDeclensions = (textForms: TextForms) => (count: number | string) => {
-  const n = Math.abs(Number(count)) % 100;
-  const n1 = n % 10;
-  if (n > 10 && n < 20) { return textForms[2]; }
-  if (n1 > 1 && n1 < 5) { return textForms[1]; }
-  if (n1 === 1) { return textForms[0]; }
-  return textForms[2];
-};
-
-export const getWordForScores = createDeclensions(['балл', 'балла', 'баллов']);
+export const SCORES_WORDS = createDeclensions(['балл', 'балла', 'баллов']);

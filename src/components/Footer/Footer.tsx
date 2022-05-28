aca6b97fc38bfef80e8566ffd3594090a51d80f7
deivoff <deivoff@gmail.com>
2022-05-28 09:19:48 +0500

    change link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,9 +3,9 @@ import { Layout } from 'antd';
 
 import s from './Footer.module.sass';
 
-const link = 'https://vk.com/mazurov.official';
-const Footer = () => (
-  <Layout.Footer className={s.footer}>Разработано <a href={link} target="_blank" rel="noreferrer">Евгением Мазуровым</a> ©2020</Layout.Footer>
-);
+const link = 'https://vk.com/deivoff';
+function Footer() {
+  return <Layout.Footer className={s.footer}>Разработано <a href={link} target="_blank" rel="noreferrer">Евгением Мазуровым</a> ©2020</Layout.Footer>;
+}
 
 export default Footer;
